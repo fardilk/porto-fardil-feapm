f2ce50820b666aec52deb645ebc176561d31e44a
Hasan Nasrullah <hnasrul22@gmail.com>
2024-07-23 16:30:27 +0700

    feature: error alert on form booking

--- src/pages/encounter/components/information-company-employee-data.tsx
+++ src/pages/encounter/components/information-company-employee-data.tsx
@@ -35,6 +35,9 @@ const InformationCompanyEmployeeData = (props: InformationCompanyEmployeeDataPro
 
 export default InformationCompanyEmployeeData
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> edd438e (feature: error alert on form booking)
+=======
+>>>>>>> 0a88b18 (feature: error alert on form booking)
