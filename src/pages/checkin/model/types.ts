79b113d155e8a3e3e28318fe06dba5712c780a86
zahrotuladillah <72771774+zahrotuladillah@users.noreply.github.com>
2024-09-06 15:21:03 +0700

    implement bookingGet

--- src/pages/checkin/model/types.ts
+++ src/pages/checkin/model/types.ts
@@ -1,45 +1,81 @@
-import { Nullable } from "src/types/common";
+import { Nullable } from 'src/types/common';
 
 export type InsertBookingProps = {
   errorMessage?: string;
 };
 
-export type CheckinResponse = {
-  booking: {
-    bookingID: string;
-    payplanClass: "GENERAL" | "BPJS" | "INSURANCE" | "COMPANY";
-    notes: string;
-    bpjs: Nullable<{
-      performerServiceName: string;
-      referralDate: string;
-      referralNumber: string;
-      subscriberCategory: string;
-      subscriberClass: string;
-      subscriberInstitution: string;
-      subscriberNumber: string;
-      subscriberStatus: string;
-    }>;
-    patient: {
-      nik: string;
-      name: string;
-      birthDttm: string;
-      birthPlace: string;
-      phone: string;
-      email: string;
-      address: string;
-      gender: string;
-      bloodType: string;
-      bloodRhesus: string;
-    };
-    encounter: {
-      healthcareServiceName: string;
-      practitionerName: string;
-      scheduleSlotDate: string;
-      scheduleSlotStartTime: string;
-    };
-  }
+// export type CheckinResponse = {
+
+// }
+
+export type BookingType = {
+  bookingID: string;
+  bookingNumber: string;
+  notes: string;
+  encounter: Encounter;
+  patient: Patient;
+  bpjs?: Bpjs;
 };
 
-export type InformationType = {
-  data: CheckinResponse
+export type Encounter = {
+  healthcareServiceName: string;
+  practitionerName: string;
+  scheduleSlotDate: string;
+  package?: Package;
 };
+
+export type Package = {
+  packageID: string;
+  packageName: string;
+};
+
+export type Bpjs = {
+  subscriberNumber: string;
+  subscriberClass: string;
+  subscriberCategory: string;
+  subscriberInstitution: string;
+  subscriberStatus: string;
+  referralNumber: string;
+  referralDate: string;
+  performerServiceName: string;
+};
+
+export type Patient = {
+  patientID: string;
+  identifierTypeCode?: string;
+  identifierValue?: string;
+  medrec: string;
+  name: string;
+  gender: string;
+  religion: string;
+  birthPlace: string;
+  birthDttm: string;
+  maritalStatus: string;
+  phone: string;
+  email: string;
+  nationality: string;
+  address: string;
+  additional?: PatientAdditionalData;
+};
+
+export type PatientAdditionalData = {
+  bloodType: string;
+  bloodRhesus: string;
+  education: string;
+  occupation: string;
+  dailyLanguage: string;
+};
+
+type PayorDetailsInsurance = {
+  policyNo: string;
+  payplanName: string;
+  providerName: string;
+};
+
+type PayorDetailsCompany = {
+  employeeNo: string;
+  payplanName: string;
+  companyName: string;
+};
+
+type PayorDetails = PayorDetailsInsurance | PayorDetailsCompany;
