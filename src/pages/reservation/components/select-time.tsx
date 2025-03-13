c2a357112bc3277cd49fec0b8e979c8ed4a17930
sangpenciptajs <ruby@sismedika.com>
2025-03-13 09:07:16 +0700

    fix wording in walkin and reservation

--- src/pages/reservation/components/select-time.tsx
+++ src/pages/reservation/components/select-time.tsx
@@ -91,7 +91,7 @@ const SelectTime = (props: SelectTimeProps) => {
               <Grid container spacing={1}>
                 <Grid item xs={12} md={4}>
                   <Box>
-                    <Typography variant='h6'>Publikasi / Seminar Kerja</Typography>
+                    <Typography variant='h6'>Dokumen Dokter</Typography>
                     <List>
                       {
                         data?.data?.doctorProfile?.doctorDocument?.map((row, index) => {
